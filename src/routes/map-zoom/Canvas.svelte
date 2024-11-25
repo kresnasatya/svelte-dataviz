41835d46d9df7a2086eb8cf260639e490f990354
Kresna Satya <kresnasatya72@gmail.com>
2024-11-26 06:14:02 +0800

    fix: set scaleCanvas in map on canvas, remove tweened, and add pendingInvalidation for invalidate requestAnimationFrame

--- src/routes/map-zoom/Canvas.svelte
+++ src/routes/map-zoom/Canvas.svelte
@@ -5,42 +5,62 @@
 	import { SvelteSet } from "svelte/reactivity";
 
     let { width, height, transform, children, hoveredColorId = $bindable(), hidden = false } = $props();
-    
+
     let canvas;
     let frameId;
     let ctx;
+    let pendingInvalidation = false;
+    let devicePixelRatio = 1;
 
     let items = new SvelteSet();
 
     setContext('canvas', { addItem });
-    const tTransform = tweened(transform, { duration: 400, easing: cubicInOut });
     
     function addItem(fn) {
         $effect(() => {
             items.add(fn);
+            invalidate();
             return () => items.delete(fn);
         });
     }
+    
+    function invalidate() {
+        if (pendingInvalidation) return;
+        pendingInvalidation = true;
+        frameId = requestAnimationFrame(update);
+    }
 
     function update() {
+        if (!ctx) return;
+
         ctx.clearRect(0, 0, width, height);
+
         items.forEach(fn => {
             ctx.save();
-            ctx.translate($tTransform.x, $tTransform.y);
-			ctx.scale($tTransform.k, $tTransform.k);
+            ctx.translate(transform.x, transform.y);
+			ctx.scale(transform.k, transform.k);
             fn(ctx);
             ctx.restore();
         });
-        frameId = requestAnimationFrame(update);
+        
+        pendingInvalidation = false;
+    }
+
+    function scaleCanvas(canvas, ctx, width, height) {
+        devicePixelRatio = Math.max(2, window ? (window.devicePixelRatio || 1) : 1);
+
+        canvas.width = width * devicePixelRatio;
+        canvas.height = height * devicePixelRatio;
+        canvas.style.width = width + 'px';
+        canvas.style.height = height + 'px';
+
+        ctx.scale(devicePixelRatio, devicePixelRatio);
     }
 
     $effect(() => {
         ctx = canvas.getContext('2d', { willReadFrequently: true });
 
         update();
-
-        tTransform.set(transform);
-        // if ($tTransform) update();
 
         return () => {
             if (frameId) {
@@ -49,9 +69,13 @@
         }
     });
 
+    $effect(() => {
+        if (canvas && ctx) scaleCanvas(canvas, ctx, width, height);
+    });
+
     function handleMouseMove(e) {
         const { layerX, layerY } = e;
-        const { data } = ctx.getImageData(layerX, layerY, 1, 1);
+        const { data } = ctx.getImageData(layerX * devicePixelRatio, layerY * devicePixelRatio, 1, 1);
         const [ r, g, b ] = data;
         if (r+g+b === 0) {
             hoveredColorId = undefined;
